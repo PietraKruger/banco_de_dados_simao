21a6e143e51b2adf92c59969a41d833966ae090b
PietraKruger <Pietrapikruger11@gmail.com>
2026-03-13 10:25:47 -0300

    criando projeto
    começando a estilizar

---
+++ js/index.js
@@ -0,0 +1,6 @@
+function mostrarDados(){
+       let nome = document.getElementById("nome").value;
+       let dataNasc = document.getElementById("dataNasc").value;
+       let email = document.getElementById("email").value;
+       let cidade = document.getElementById("cidade").value;
+    }
