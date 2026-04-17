242cec3bd318736f0ed6aa0532da3d5156999070
PietraKruger <Pietrapikruger11@gmail.com>
2026-04-17 09:39:55 -0300

    mais um exercicio

---
+++ exercicios java/estruturaCondicional.java
@@ -0,0 +1,17 @@
+import java.util.scanner;
+
+public class EstruturaCondicional {
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o valor da variavel x:");
+
+        int x = scanner.nextInt();
+
+        System.out.print("Digite o valor da variavel y:");
+
+        int y = scanner.nextInt();
+    }
+}
