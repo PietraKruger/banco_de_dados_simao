af5485acd2b694ebaff156498c7736820da65a84
PietraKruger <Pietrapikruger11@gmail.com>
2026-04-17 09:37:44 -0300

    exercicios novos

---
+++ exercicios java/exercicioString.java
@@ -0,0 +1,15 @@
+public class ExercicioString {
+
+    public static void main(String[] args) {
+     
+        String nome = "Pietra";
+
+        String idade = "16";
+
+        String bairro = "Santo Antônio de Lisboa";
+
+        System.out.println( "Meu nome é " + nome);
+        System.out.println(" minha idade é " + idade );
+        System.out.println(" moro no bairro " + bairro);
+    }
+}
---
+++ exercicios java/exerciciosVariaveis.java
@@ -0,0 +1,24 @@
+public class ExerciciosVariaveis {
+
+public static void main(String[] args) {
+
+    int x = 10;
+
+    int y = 20; 
+
+    int adicao = x+y;
+
+    int subtracao = x-y;
+
+    int multiplicacao = x*y;
+
+    int divisao = x/y;
+
+    System.out.println (adicao);
+    System.out.println (subtracao);
+    System.out.println (multiplicacao);
+    System.out.println (divisao);
+    
+    
+}
+    }
